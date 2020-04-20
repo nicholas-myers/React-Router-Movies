cdfa224cbb3b8b29363068e358bade9170ba49e7
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-04-20 13:17:25 -0700

    removed axios from movielist and injected into Appjs

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,31 @@
-import React, { useState } from "react";
-import { Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Route, Switch } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie"
+import axios from 'axios';
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
   // set state for movies list
+  const [movies, setMovies] = useState([])
+  useEffect(() => {
+    const getMovies = () => {
+      axios
+        .get('http://localhost:5000/api/movies')
+        .then(response => {
+          setMovies(response.data);
+          // console.log(response.data);
+        })
+        .catch(error => {
+          console.error('Server Error', error);
+        });
+    }
+    
+    getMovies();
+  }, []);
+
+
   const addToSavedList = (movie) => {
     setSavedList([...savedList, movie]);
   };
@@ -15,12 +34,16 @@ const App = () => {
     <div>
       <SavedList list={savedList} />
       <div>
+    
         <Route path="/">
-          <MovieList />
+          <MovieList movies={movies}/>
         </Route>
         <Route path="/movies/:id">
           <Movie />
         </Route>
+        
+        
+        
       </div>
     </div>
   );
