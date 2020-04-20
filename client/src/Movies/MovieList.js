8b301d5a0715151252bb399480e1cc3ac6036fc0
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-04-20 13:27:29 -0700

    turned each card back into a link

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,15 +1,30 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { Link } from "react-router-dom"
+import axios from 'axios';
 
-
-const MovieList = ({ movies }) => {
+const MovieList = (props) => {
+  const [movies, setMovies] = useState([])
+  useEffect(() => {
+    const getMovies = () => {
+      axios
+        .get('http://localhost:5000/api/movies')
+        .then(response => {
+          setMovies(response.data);
+        })
+        .catch(error => {
+          console.error('Server Error', error);
+        });
+    }
+    
+    getMovies();
+  }, []);
   
   return (
     <div className="movie-list">
       {movies.map(movie => (
-          <Link to={`/movies/${movie.id}`}>
-            <MovieDetails key={movie.id} movie={movie} />
-          </Link>
+        <Link to={`/movies/${movie.id}`}>
+          <MovieDetails key={movie.id} movie={movie} />
+        </Link>
       ))}
     </div>
   );
